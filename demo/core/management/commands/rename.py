07d34a4a37787c248851f89f473b2d7c3d74b509
aakash2410 <58226995+aakash2410@users.noreply.github.com>
2020-11-06 15:25:49 +0530

    fixed renaming issues

--- demo/core/management/commands/rename.py
+++ demo/core/management/commands/rename.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
         new_project_name = kwargs['new_project_name']
 
         files_to_rename = [new_project_name+ '/settings/base.py', new_project_name + '/wsgi.py', 'manage.py']
-        folder_to_rename = 'demo'
+        folder_to_rename = 'myproject'
         os.rename(folder_to_rename,new_project_name)
         for f in files_to_rename:
             with open(f, 'r') as file:
                 filedata = file.read()
 
-            filedata = filedata.replace('demo', new_project_name)
+            filedata = filedata.replace('myproject', new_project_name)
 
             with open(f, 'w') as file:
                 file.write(filedata)
